2f95728929e640821164f958018a2192e53fe2e4
thautwarm <twshere@outlook.com>
2018-11-18 22:32:02 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import rbnfrbnf
+readme = ""
+
+setup(
+    name='rbnfrbnf',
+    version=rbnfrbnf.__version__,
+    keywords='parser generation, LR parser, efficient, JIT',
+    description='A best LR parser generator',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    python_requires='>=3.6.0',
+    url='https://github.com/thautwarm/rbnfrbnf',
+    author='thautwarm, lfkdsk',
+    author_email='twshere@outlook.com',
+    packages=find_packages(),
+    # entry_points={'console_scripts': ['yapypy=yapypy.cmd.cli:python_ex_cli']},
+    install_requires=['rbnf'],
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    zip_safe=False)
